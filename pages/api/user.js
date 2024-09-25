02d35ed3bdbe7b3cdffaa5d6f6da0710ec8843c5
Saman <samanezzatabadi@yahoo.com>
2024-09-25 11:29:36 +0330

    fixed

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,15 +1,25 @@
 import User from "@/models/User";
 import { verifyToken } from "@/utils/auth";
+import connectDb from "@/utils/connectDB";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") return;
+
+  try {
+    await connectDb();
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "failed", message: "error in connecting to dataBase" });
+  }
 
   const { token } = req.cookies;
 
   const isValidate = verifyToken(token, process.env.SECRET_KEY);
 
   const user = await User.findOne({ email: isValidate.email });
-  
+
   if (!user) {
     return res
       .status(404)
@@ -17,13 +27,11 @@ export default async function handler(req, res) {
   }
 
   if (isValidate) {
-    return res
-      .status(200)
-      .json({
-        status: "success",
-        message: "you are logged in",
-        userName: user.name,
-      });
+    return res.status(200).json({
+      status: "success",
+      message: "you are logged in",
+      userName: user.name,
+    });
   } else {
     return res.status(401).json({ status: "failed", message: "not log in" });
   }
